821ccd7f4b16068c565b017f9282a8671402d4d9
AshKiya <52596969+AshKiya@users.noreply.github.com>
2020-02-17 18:55:58 +0530

    Update insert.php

--- pintu/insert.php
+++ pintu/insert.php
@@ -10,18 +10,6 @@ if(isset($_POST['submit']))
     $password=mysqli_real_escape_string($connection , $_POST['password']);
     
     
-    if($name==""){
-        echo "<script>alert(' Enter Your Name ');</script>";
-    }else if($contact==""){
-        echo "<script>alert(' Enter Your Mobile Number ');</script>";
-    }else if($accnum==""){
-        echo "<script>alert(' Enter Your Account Number ');</script>";
-    }else if($username==""){
-        echo "<script>alert(' Enter Your Email ID ');</script>";
-    }else if($password==""){
-        echo "<script>alert(' Enter Password ');</script>";
-    }else{
-    
     $sql="INSERT INTO `bank_account` (`id`, `name`, `contact`, `accnum`, `username`, `password`, `date`) VALUES (NULL, '$name', '$contact', '$accnum', '$username', '$password', now());";
     $query=mysqli_query($connection , $sql);
     if($query){
@@ -29,7 +17,7 @@ if(isset($_POST['submit']))
     }else{
         echo "Something Went Wrong";
     }
-    }
+    
 }
 
 ?>
