b582bd4bfd6b0335dd78627a8e711b528cae8e7b
AshKiya <52596969+AshKiya@users.noreply.github.com>
2020-02-17 19:03:53 +0530

    Update insert.php

--- pintu/insert.php
+++ pintu/insert.php
@@ -3,12 +3,17 @@ include ('dbconn.php');
 
 if(isset($_POST['submit']))
 {
-    $name=mysqli_real_escape_string($connection , $_POST['name']);
-    $contact=mysqli_real_escape_string($connection , $_POST['contact']);
-    $accnum=mysqli_real_escape_string($connection , $_POST['accnum']);
-    $username=mysqli_real_escape_string($connection , $_POST['username']);
-    $password=mysqli_real_escape_string($connection , $_POST['password']);
+//     $name=mysqli_real_escape_string($connection , $_POST['name']);
+//     $contact=mysqli_real_escape_string($connection , $_POST['contact']);
+//     $accnum=mysqli_real_escape_string($connection , $_POST['accnum']);
+//     $username=mysqli_real_escape_string($connection , $_POST['username']);
+//     $password=mysqli_real_escape_string($connection , $_POST['password']);
     
+    $name= $_POST['name'];
+    $contact= $_POST['contact'];
+    $accnum=$_POST['accnum'];
+    $username= $_POST['username'];
+    $password= $_POST['password'];
     
     $sql="INSERT INTO `bank_account` (`id`, `name`, `contact`, `accnum`, `username`, `password`, `date`) VALUES (NULL, '$name', '$contact', '$accnum', '$username', '$password', now());";
     $query=mysqli_query($connection , $sql);
