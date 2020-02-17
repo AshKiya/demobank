1ef83690ca4dbf690a04431a6896ee8d59ea936e
AshKiya <52596969+AshKiya@users.noreply.github.com>
2020-02-17 18:20:57 +0530

    Update dbconn.php

--- pintu/dbconn.php
+++ pintu/dbconn.php
@@ -1,17 +1,16 @@
 <?php
-session_start();
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
 $dbuser = getenv("databaseuser");
 $dbpwd = getenv("databasepassword");
 $dbname = getenv("databasename");
 
-$connection = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
-if ($connection) {
-    echo ("Connect failed");
+$connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+if ($connection->connect_errno) {
+    printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 } else {
-    echo ("Connected to the database");
+    printf("Connected to the database");
 }
 $connection->close();
 ?>
