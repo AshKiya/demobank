5657b547b63366741042e70f7a6b346dd208f26f
AshKiya <52596969+AshKiya@users.noreply.github.com>
2020-02-18 16:58:22 +0530

    Update dbconn.php

--- pintu/dbconn.php
+++ pintu/dbconn.php
@@ -1,9 +1,16 @@
 <?php
-$dbhost = getenv("MYSQL_SERVICE_HOST");
-$dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("databaseuser");
-$dbpwd = getenv("databasepassword");
-$dbname = getenv("databasename");
+// $dbhost = getenv("MYSQL_SERVICE_HOST");
+// $dbport = getenv("MYSQL_SERVICE_PORT");
+// $dbuser = getenv("databaseuser");
+// $dbpwd = getenv("databasepassword");
+// $dbname = getenv("databasename");
+
+
+$dbhost = "mysql";
+
+$dbuser = "databaseuser";
+$dbpwd = "databasepassword";
+$dbname = "databasename";
 
 $connection = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname);
 if ($connection) {
